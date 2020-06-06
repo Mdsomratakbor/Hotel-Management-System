d8dd5100310d7e0b9a331f4ede2176aae17dabc0
Mdsomratakbor <somrat876@gmail.com>
2020-06-06 13:28:15 +0600

    AccomodationPackagesListingModel and AccomodationPackagesModel Some Propetise add

--- HMS.Web/Areas/Dashboard/ViewModels/AccomodationPackagesModels.cs
+++ HMS.Web/Areas/Dashboard/ViewModels/AccomodationPackagesModels.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HMS.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,9 +8,18 @@ namespace HMS.Web.Areas.Dashboard.ViewModels
 {
     public class AccomodationPackagesListingModel
     {
+        public List<AccomodationPackage> AccomodationPackages { get; set; }
+        public string SearchTerm { get; set; }
+        public int PageSize { get; set; }
+        public int PageNo { get; set; }
+        public Pager Pager { get; set; }
     }
     public class AccomodationPackagesModel
     {
-
+        public int ID { get; set; }
+        public int AccomodationTypeID { get; set; }
+        public AccomodationType AccomodationType { get; set; }
+        public string NoOfRoom { get; set; }
+        public decimal FeePerNight { get; set; }
     }
 }
