60b7e81206753f95ff377a49e0d4cbcaa3929ea7
Mdsomratakbor <somrat876@gmail.com>
2020-06-07 22:26:14 +0600

    AccomodationModels Class Add in ViewModels folder

---
+++ HMS.Web/Areas/Dashboard/ViewModels/AccomodationModels.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HMS.Web.Areas.Dashboard.ViewModels
+{
+    public class AccomodationModels
+    {
+    }
+}
