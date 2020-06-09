65589d5622e118fbeb64b379484e7c69fbaad0ab
Mdsomratakbor <somrat876@gmail.com>
2020-06-09 20:57:11 +0600

    Add HMSSignManager Class

---
+++ HMS.Services/HMSSignInManagerService.cs
@@ -0,0 +1,31 @@
+﻿using HMS.Entities;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Services
+{
+    public class HMSSignInManagerService : SignInManager<HMSUser, string>
+    {
+        public HMSSignInManagerService(HMSUserManagerService userManager, IAuthenticationManager authenticationManager)
+            : base(userManager, authenticationManager)
+        {
+        }
+
+        public override Task<ClaimsIdentity> CreateUserIdentityAsync(HMSUser user)
+        {
+            return user.GenerateUserIdentityAsync((HMSUserManagerService)UserManager);
+        }
+
+        public static HMSSignInManagerService Create(IdentityFactoryOptions<HMSSignInManagerService> options, IOwinContext context)
+        {
+            return new HMSSignInManagerService(context.GetUserManager<HMSUserManagerService>(), context.Authentication);
+        }
+    }
+}
