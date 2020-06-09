2c57f62a53126d206c9427ff59ff63055d1185a5
Mdsomratakbor <somrat876@gmail.com>
2020-06-09 21:43:43 +0600

    UsersController Some Change and UserModels Some Change

--- HMS.Web/Areas/Dashboard/Controllers/UsersController.cs
+++ HMS.Web/Areas/Dashboard/Controllers/UsersController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using HMS.Services;
+using HMS.Web.Areas.Dashboard.ViewModels;
+using Microsoft.AspNet.Identity.Owin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,18 +11,163 @@ namespace HMS.Web.Areas.Dashboard.Controllers
 {
     public class UsersController : Controller
     {
+        private HMSSignInManagerService _signInManager;
+        private HMSUserManagerService _userManager;
+
+        public UsersController()
+        {
+        }
+
+        public UsersController(HMSUserManagerService userManager, HMSSignInManagerService signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        public HMSSignInManagerService SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<HMSSignInManagerService>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        public HMSUserManagerService UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<HMSUserManagerService>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
         // GET: Dashboard/Users
         public ActionResult Index()
         {
             return View();
         }
-        public ActionResult Listing()
+        public PartialViewResult Listing(string searchTearm, int? accomodationPackageId, int? pageNo, int? pageSize)
         {
-            return View();
+            UserListingModel model = new UserListingModel();
+            pageNo = pageNo ?? 1;
+            pageSize = pageSize ?? 10;
+            model.Users = UserManager.Users;
+            //model.AccomodatioPackages = _AccomodationPackagesService.GetAllAccomodationPackage();
+            int totalItems = 0; //_AccomodationService.TotalItemCount(searchTearm, accomodationPackageId);
+            model.Pager = new Pager(totalItems, pageNo, pageSize.Value);
+            model.SearchTerm = searchTearm;
+            model.PageNo = pageNo.Value;
+            model.PageSize = pageSize.Value;
+            return PartialView("_Listing", model);
         }
-        public ActionResult Action()
+        [HttpGet]
+        public PartialViewResult Action(int? id)
         {
-            return View();
+            AccomodationModel model = new AccomodationModel();
+            if (id.HasValue)
+            {
+                //_Accomodation = _AccomodationService.GetAccomodationById(id.Value);
+                //model.ID = _Accomodation.ID;
+                //model.Name = _Accomodation.Name;
+                //model.AccomodationPackageID = _Accomodation.AccomodationPackageID;
+                //model.Description = _Accomodation.Description;
+            }
+            //model.AccomodationPackage = _AccomodationPackagesService.GetAllAccomodationPackage();
+
+            return PartialView("_Action", model);
+        }
+        [HttpPost]
+        public JsonResult Action(AccomodationModel model)
+        {
+            JsonResult result = new JsonResult();
+            result.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+            var message = "";
+            bool data = false;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if (model.ID > 0)
+                    {
+                        //_Accomodation = _AccomodationService.GetAccomodationById(model.ID);
+                        //_Accomodation.Name = model.Name;
+                        //_Accomodation.Description = model.Description;
+                        //_Accomodation.AccomodationPackageID = model.AccomodationPackageID;
+                        //data = _AccomodationService.UpdateAccomodation(_Accomodation);
+                    }
+                    else
+                    {
+                        //_Accomodation.ID = model.ID;
+                        //_Accomodation.Name = model.Name;
+                        //_Accomodation.Description = model.Description;
+                        //_Accomodation.AccomodationPackageID = model.AccomodationPackageID;
+                        //data = _AccomodationService.SaveAccomodation(_Accomodation);
+                    }
+
+                }
+                else
+                {
+                    message = "Please enter valid data!!";
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+            }
+            if (data)
+            {
+                message = "Data Save Successfully!!";
+                result.Data = new { Success = true, Message = message };
+            }
+            else
+            {
+                result.Data = new { Success = false, Message = message };
+            }
+            return result;
+
+        }
+
+        public JsonResult Delete(int id)
+        {
+            JsonResult result = new JsonResult();
+            result.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+            dynamic message = "";
+            var data = false;
+            try
+            {
+                if (id > 0)
+                {
+                    //_Accomodation = _AccomodationService.GetAccomodationById(id);
+                    //data = _AccomodationService.DeleteAccomodation(_Accomodation);
+                }
+                else
+                {
+                    message = "Please click valid item";
+                }
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+            }
+            if (data)
+            {
+                message = "Data Delete Successfully !!";
+                result.Data = new { Success = true, Message = message };
+            }
+            else
+            {
+                result.Data = new { Success = false, Message = message };
+            }
+
+            return result;
         }
     }
 }
--- HMS.Web/Areas/Dashboard/ViewModels/UserModels.cs
+++ HMS.Web/Areas/Dashboard/ViewModels/UserModels.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HMS.Entities;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,9 +9,22 @@ namespace HMS.Web.Areas.Dashboard.ViewModels
 {
     public class UserListingModel
     {
+        public IEnumerable<HMSUser> Users { get; set; }
+        public string RoleID { get; set; }
+        public IEnumerable<IdentityRole> Roles { get; set; }
+        public string SearchTerm { get; set; }
+        public int PageSize { get; set; }
+        public int PageNo { get; set; }
+        public Pager Pager { get; set; }
+
     }
     public class UserModel
     {
+        public int ID { get; set; }
+        public string RoleID { get; set; }
+        public IdentityRole Role { get; set; }
+        public IEnumerable<IdentityRole> Roles { get; set; }
+        public string Name { get; set; }
 
     }
 }
