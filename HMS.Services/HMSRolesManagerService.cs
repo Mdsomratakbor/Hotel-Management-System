f1830551513dd72eaf5d4c5b1c239360acc46818
Mdsomratakbor <somrat876@gmail.com>
2020-06-10 20:20:29 +0600

    HMSRolesManagerService some changes

--- HMS.Services/HMSRolesManagerService.cs
+++ HMS.Services/HMSRolesManagerService.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using HMS.Data;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,15 @@ using System.Threading.Tasks;
 
 namespace HMS.Services
 {
-    public class HMSRolesManagerService
+    public class HMSRolesManagerService :RoleManager<IdentityRole>
     {
+        public HMSRolesManagerService(IRoleStore<IdentityRole, string> roleStore):base(roleStore)
+        {
+            
+        }
+        public static HMSRolesManagerService Create(IdentityFactoryOptions<HMSRolesManagerService>options, IOwinContext context)
+        {
+            return new HMSRolesManagerService(new RoleStore<IdentityRole>(context.Get<HMSContext>()));
+        }
     }
 }
