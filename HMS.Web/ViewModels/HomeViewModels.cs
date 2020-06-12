21002e4f2350178d4c2f27bf66395528a8b9e33a
Mdsomratakbor <somrat876@gmail.com>
2020-06-12 23:08:17 +0600

    Add folder ViewModels and Add class HomeViewModel

---
+++ HMS.Web/ViewModels/HomeViewModels.cs
@@ -0,0 +1,13 @@
+﻿using HMS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HMS.Web.ViewModels
+{
+    public class HomeViewModel
+    {
+        public List<AccomodationType> AccomodationTypes { get; set; }
+    }
+}
