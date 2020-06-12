1608a4e9bd054068a39d9afde05a97830fb08dbe
Mdsomratakbor <somrat876@gmail.com>
2020-06-13 00:23:38 +0600

    AccomodationDetailsController add

---
+++ HMS.Web/Controllers/AccomodationDetailsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HMS.Web.Controllers
+{
+    public class AccomodationDetailsController : Controller
+    {
+        // GET: AccomodationDetails
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
