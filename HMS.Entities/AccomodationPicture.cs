2b42b3a8af82a4822bb0c083f8ac190945e0a25e
Mdsomratakbor <somrat876@gmail.com>
2020-06-13 22:16:05 +0600

    AccomodationPicture class Entity add

--- HMS.Entities/AccomodationPicture.cs
+++ HMS.Entities/AccomodationPicture.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace HMS.Entities
 {
-    public class AccomodationPackagePicture
+   public class AccomodationPicture
     {
         public int ID { get; set; }
         public int AccomodationPackageID { get; set; }
