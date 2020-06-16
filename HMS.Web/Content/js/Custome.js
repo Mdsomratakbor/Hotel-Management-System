05e0bb6875c527852a0a9c13126610203e5e7308
Mdsomratakbor <somrat876@gmail.com>
2020-06-17 00:35:50 +0600

    Custom.js file add and accomodation package show by accomodation Type

---
+++ HMS.Web/Content/js/Custome.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () { 
+$(".changeAccomodationType").on('click', function () {
+    var accomodationTypeId = $(this).attr('data-id');
+    $('.accomodationTypesRow').hide();
+    $('div.accomodationTypesRow[data-id =' + accomodationTypeId + ']').show('slow');
+})
+
+
+})
